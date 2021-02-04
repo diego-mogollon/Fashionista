586ff350a13396d09e214935fc2b20e0dbb88d9e
Aparna Achuthan <appu4ever@gmail.com>
2021-02-04 21:21:03 +1100

    Items CRUD done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,7 @@ class ItemsController < ApplicationController
 
     def create 
       @item = Item.new(item_params)
+      # @item.user = current_user
       if @item.save
         redirect_to item_path(@item)
       else
@@ -20,13 +21,26 @@ class ItemsController < ApplicationController
       end
     end
 
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
+    end
+
     def destroy
       @item = Item.find(params[:id])
       @item.destroy
-      redirect_to items
+      redirect_to root_path
     end
-    private
 
+    private
     def item_params
       params.require(:item).permit(:name, :price, :size, :description)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:show, :new, :create]
+  resources :items, only: [:show, :new, :create, :edit, :update, :destroy]
 end
