d5f25dbcc6d85258d53062d81bcbf69075a865a7
anil pak <anilpak35@gmail.com>
2021-02-05 21:27:46 +1100

    booking controller completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,43 @@
 class BookingsController < ApplicationController
-  def new
-    @item = Item.find(params[:item_id])
-    @booking = Booking.new
-  end
-    
   def create
     @item = Item.find(params[:item_id])
     @booking = Booking.new(booking_params)
-    @booking.item = @booking
-    @booking.save
-    redirect_to item_path(@item)
+    @booking.item = @item
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render new
+    end
   end
-    
+
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @item = @booking.item
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.save!
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to root_path
+  end
+
   private
-    
+
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:monster_id, :user_id, :start_date, :end_date, :total_price)
   end
 end
