ae472c342df3b49b831b6c129582cc08140a9729
anil pak <anilpak35@gmail.com>
2021-02-05 20:47:54 +1100

    booking controllers created. Also html file created for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def new
+    @item = Item.find(params[:item_id])
+    @booking = Booking.new
+  end
+    
+  def create
+    @item = Item.find(params[:item_id])
+    @booking = Booking.new(booking_params)
+    @booking.item = @booking
+    @booking.save
+    redirect_to item_path(@item)
+  end
+    
+  private
+    
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
