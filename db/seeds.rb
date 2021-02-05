1a604e01e5b7a748096f2ca4859e2e0de5a95208
anil pak <anilpak35@gmail.com>
2021-02-05 18:31:01 +1100

    items index and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts "database is clean"
 puts 'Creating 1 user...'
 user_1 = User.create(email: "user@user.com.tr", password: 111111)
 puts "#{User.count} User created..."
-puts 'Creating 3 items...'
+puts 'Creating 1 items...'
 
 Item.create(name: 'Chin Chin', price: rand(30..50), description: "Phasellus pretium mauris erat, at egestas massa accumsan vel.", size: "XS", user_id: user_1.id )
 
