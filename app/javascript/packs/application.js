23f928f92e2c9aa4b5ed1ebd7d4340f5e285090a
anil pak <anilpak35@gmail.com>
2021-02-05 20:32:42 +1100

    javascript and css settings done for flatpickr plugin also inside the javascript folder, pluins folder created and booking plugin in the flatpickr.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
-import flatpickr from "flatpickr";
-import "flatpcikr/dist/flatpickr.min.js";
-
-flatpickr(".datepicker", {});
+import "plugins/flatpickr";
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,7 @@
+import flatpickr from "flatpickr";
+import "flatpcikr/dist/themes/confetti.css";
+
+flatpickr(".datepicker", {
+  altInput = true,
+  enableTime = true
+});
