bd66c607dedd85d9ff47b12bc21d148422c9d4fa
anil pak <anilpak35@gmail.com>
2021-02-06 12:47:36 +1100

    create booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,14 +16,14 @@ class BookingsController < ApplicationController
     end
   end
 
-  # def index
-  #   @bookings = Booking.where(user_id: current_user.id)
-  # end
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
 
-  # def show
-  #   @booking = Booking.find(params[:id])
-  #   @item = @booking.item
-  # end
+  def show
+    @booking = Booking.find(params[:id])
+    @item = @booking.item
+  end
 
   # def update
   #   @booking = Booking.find(params[:id])
