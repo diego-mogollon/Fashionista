743b1b56af961985ac09443facaf4301b368c9d8
diego mogollon <diego.mogollon.solano@gmail.com>
2021-02-06 14:13:23 +1100

    Added relations between bookings, user and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :bookings
+  has_many :reviews, :through => :bookings, dependent: :destroy
 end
