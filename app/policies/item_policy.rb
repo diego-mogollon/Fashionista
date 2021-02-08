bb518d4bb13dac3ae577fabb7ed64f03a02d2f5d
anil pak <anilpak35@gmail.com>
2021-02-08 13:33:23 +1100

    items_controller updated according to item policy and item policy methods created

---
+++ app/policies/item_policy.rb
@@ -0,0 +1,19 @@
+class ItemPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def update?
+    user == record.user
+  end
+end
--- test/policies/item_policy_test.rb
+++ test/policies/item_policy_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RestaurantPolicyTest < ActiveSupport::TestCase
+class ItemPolicyTest < ActiveSupport::TestCase
   def test_scope
   end
 
