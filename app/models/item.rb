c7949684c29e5202ee3468528188db820b39b3d7
diego mogollon <diego.mogollon.solano@gmail.com>
2021-02-09 20:44:06 +1100

    Added validation for all required fileds in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,10 @@ class Item < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, :through => :bookings, dependent: :destroy
   has_many :categories, through: :item_categories
+  validates :name, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 
   def unavailable_dates
     bookings.pluck(:start_date, :end_date).map do |range|
