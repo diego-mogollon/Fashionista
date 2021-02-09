a278f6443a6611211e079946125d0972e9ddc5be
anil pak <anilpak35@gmail.com>
2021-02-09 17:25:49 +1100

    before action and show method created in the dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,10 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(current_user[:id])
+    @bookings = @user.booking
+    @items = @user.item
+    @reviews = @user.review
+  end
 end
