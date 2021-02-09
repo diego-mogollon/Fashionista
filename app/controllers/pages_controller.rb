3a93410b4ca7923f3ed7f7664043c896ac0cdebb
anil pak <anilpak35@gmail.com>
2021-02-09 17:23:17 +1100

    pages controller removed instead dashboard controller created

--- app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only:[:index, :show, :home]
-  def dashboard
-    @user = current_user
-  end
-  def home
-    @categories = Category.all
-  end
-end
