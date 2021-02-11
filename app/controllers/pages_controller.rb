2fd91eab1c225227fada75aa9e0e3d533544078f
diego mogollon <diego.mogollon.solano@gmail.com>
2021-02-11 19:52:53 +1100

    updated model to has_many bookings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def dashboard
     @user = current_user
-    authorize @user
+    @user_bookings = Booking.where(user_id: @user.id)
+    # authorize @user
   end
 
   def home
     @categories = Category.all
   end
+
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   has_one :review
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
+
   private
 
   def end_date_after_start_date
